9436aa00909744c44fe9c7ef4b01375391f81fa9
mariogalansoto <mariogalansoto@hotmail.com>
2024-09-04 08:03:52 -0400

    add a big file with english words to test the code, handle anagrams by character frequency building a map of characters for each word in O(m) time, the areAnagrams function now compares 2 words by their character frequency maps, which is more efficient than sorting, and print the values of anagrams in the big file

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 
+function getCharFrequency(word){
+	const frecuency = {};
+	// for every word get the frecuency of the characters using hash maps
+	for (const char of word){
+		// if the character was not in here add it otherwise add 1 to it's count
+		frecuency[char] = (frecuency[char] || 0) + 1;
+	}
+	return frequency;
+}
+
+
 function areAnagrams(word1,word2){
 
     // Check if the lengths of the word are different; If so, they cannot be anagrams
     if (word1.length !== word2.length) {
         return false;
     }
-
-    //Convert the words to lowercase to make the comparison case-insentitive
-    word1= word1.toLowerCase();
-    word2= word2.toLowerCase();
     
     //Convert words to arrays of characters, sort them, and join them back into strings
-    const sortedWord1= word1.split('').sort().join('');
-    const sortedWord2= word2.split('').sort().join('');
+    const freq1 = getCharFrequency(word1.toLowerCase());
+    const freq2 = getCharFrequency(word2.toLowerCase());
 
     //Compare the sorted string; If they are equal, the words are anagrams
-    return sortedWord1 === sortedWord2;
+    for (const char in freq1){
+	if (freq1[char] !== freq2[char]){
+		return false;
+	}
+    }
+    return true;
 }
 
 function readWordsFile() {
-  let readWords = []
   const filePath = path.join(__dirname, 'words.txt');
   
   fs.readFile(filePath, 'utf8', (err, data) => {
@@ -31,16 +42,23 @@ function readWordsFile() {
     }
     
     const lines = data.split('\n');
-    for(let i=0;i<lines.length; i++){
-	//check if and this are anagrams
-	for(let a=i+1; a<(lines.length-1); a++){
-		if(areAnagrams(lines[i], lines[a])){
-			console.log(`${lines[i]} and ${lines[a]} are anagrams`);
-		}else{
-			console.log(`---> ${lines[i]} is not a anagram of ${lines[a]}`);
-		}
+    const anagramGroups = {};
+    //Group words by their character frequency signature
+    for (const word of lines){
+	const sortedWord = word.toLowerCase().split('').sort().join('');
+	// group and save all anagrams first, this way you dont have to check every word each time
+	if (anagramGroups[sortedWord]){
+		anagramGroups[sortedWord].push(word);
+	}else{
+		anagramGroups[sortedWord]=[word];
 	}
-	    //console.log(`Line ${i} : ${lines[i]}`);
+    }
+
+    // Print out the groups of anagrams
+    for (const group in anagramGroups){
+	if(anagramGroups[group].length > 1){
+		console.log(`Anagrams: ${anagramGroups[group].join(', ')}`)
+	}
     }
   });
 }
